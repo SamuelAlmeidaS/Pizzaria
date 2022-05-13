c9b038bf6712c0e50e8e80835c108481e446e8d7
Samuel Almeida <75258537+SamuelAlmeidaS@users.noreply.github.com>
2022-05-13 15:23:03 -0300

    Description

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
     // Preencher as informações em pizzaitem
 
+
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img; // IMAGENS PIZZAS
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`; // PREÇOS
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name; // NOMES PIZZAS
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description; // DESCRIÇÃO
+
     c('.pizza-area').append( pizzaItem );
 });
