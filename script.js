c3c3c98111dc5b970ccfb63382d21ae697750c5a
Samuel Almeida <75258537+SamuelAlmeidaS@users.noreply.github.com>
2022-05-14 10:59:18 -0300

    modal

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
-const c = (el)=>document.querySelector(el);
+let modalQt = 1;
 
+const c = (el)=>document.querySelector(el);
+const cs = (el)=>document.querySelectorAll(el);
+
+// Listagem das pizzas
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
     // Preencher as informações em pizzaitem
 
 
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img; // IMAGENS PIZZAS
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`; // PREÇOS
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name; // NOMES PIZZAS
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description; // DESCRIÇÃO
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+        let key = e.target.closest('.pizza-item').getAttribute('data-key'); 
+        modalQt = 1;
+
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = item.description;
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+
+        cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{ // forEach para cada item, rodar uma função
+            if(sizeIndex == 2){
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML =  pizzaJson[key].sizes[sizeIndex];
+        });
+
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            c('.pizzaWindowArea').style.opacity = 1;
+        });
+    });
 
     c('.pizza-area').append( pizzaItem );
+});
+
+// Eventos MODAL
+function closeModal(){
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+}
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
 });
